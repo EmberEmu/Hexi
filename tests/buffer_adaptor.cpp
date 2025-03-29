cba07f2ef9603d2e27386df4e339c685f5767136
Chaosvex <901914+Chaosvex@users.noreply.github.com>
2025-03-29 15:34:51 +0000

    Fix helper buffer_adaptor write() compile error (#10)

--- tests/buffer_adaptor.cpp
+++ tests/buffer_adaptor.cpp
@@ -89,6 +89,9 @@ TEST(buffer_adaptor, write) {
 	ASSERT_EQ(adaptor.size(), values.size());
 	ASSERT_EQ(buffer.size(), values.size());
 	ASSERT_TRUE(std::ranges::equal(values, buffer));
+	const auto size = adaptor.size();
+	adaptor.write('\0');
+	ASSERT_EQ(adaptor.size(), size + 1);
 }
 
 TEST(buffer_adaptor, write_append) {
