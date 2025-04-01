b77f960775752e463bb01f46df47c24d0c80935d
Chaosvex <ch@osvex.com>
2025-04-01 01:16:30 +0100

    Simplify range overlap check

--- single_include/hexi.h
+++ single_include/hexi.h
@@ -132,12 +132,7 @@ static inline bool region_overlap(const void* src, std::size_t src_len, const vo
 	const auto src_end = src_beg + src_len;
 	const auto dst_beg = std::bit_cast<std::uintptr_t>(dst);
 	const auto dst_end = dst_beg + dst_len;
-
-	// cannot assume src is before dst or vice versa
-	return (src_beg >= dst_beg && src_beg < dst_end)
-		|| (src_end > dst_beg && src_end <= dst_end)
-		|| (dst_beg >= src_beg && dst_beg < src_end)
-		|| (dst_end > src_beg && dst_end <= src_end);
+	return src_beg < dst_end && dst_beg < src_end;
 }
 
 } // detail
