bc0a18205b2b20e16bc84dc716a1b34de58fc359
Chaosvex <ch@osvex.com>
2026-03-24 22:22:19 +0000

    Add version defines and update notice version

---
+++ include/hexi/version.h
@@ -0,0 +1,11 @@
+//  _               _ 
+// | |__   _____  _(_)
+// | '_ \ / _ \ \/ / | MIT & Apache 2.0 dual licensed
+// | | | |  __/>  <| | Version 1.3.5
+// |_| |_|\___/_/\_\_| https://github.com/EmberEmu/hexi
+
+#pragma once
+
+#define HEXI_VERSION_MAJOR 1
+#define HEXI_VERSION_MINOR 3
+#define HEXI_VERSION_PATCH 5
--- tests/binary_stream_pmc.cpp
+++ tests/binary_stream_pmc.cpp
@@ -1,7 +1,7 @@
 //  _               _ 
 // | |__   _____  _(_)
 // | '_ \ / _ \ \/ / | MIT & Apache 2.0 dual licensed
-// | | | |  __/>  <| | Version 1.0
+// | | | |  __/>  <| | Version 1.3.5
 // |_| |_|\___/_/\_\_| https://github.com/EmberEmu/hexi
 
 #include <hexi/pmc/binary_stream.h>
